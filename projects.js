a07bf9bdb17341c758e1569915c40f64ec82d611
Devil256 <55337230+Devil256@users.noreply.github.com>
2023-06-15 18:58:06 -0400

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,30 @@
+function vietnam(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/vietnam.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = "This is a piece that I made in Photoshop to introduce the tourists about Vietnam landscapes. There are beautiful beaches, bay, terraces, or waterfall."
+}
+function summer(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/summer.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = "This is a mid-term project. I used simple objects like umbrella, star fish, bouy, etc."
+}
+function x(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/x.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = ""
+}
+function currency(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/currency.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = ""
+}
+function h(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/h.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = ""
+}
+function i(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/i.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = ""
+}
+function mypic(){
+    document.getElementById("right").getElementsByTagName("img")[0].src = "./images/mypic.jpg"
+    document.getElementById("right").getElementsByTagName("p")[0].innerHTML = ""
+}
+
+
